509d4e9bde65fdea653731d5521e016a80b1b48a
Michael Petro <mgpetro99@gmail.com>
2021-03-01 08:56:32 -0500

    Added license and make setup changes

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/michaelmicheal/PythonVehicleAPIWrapper",
+    install_requires=["pandas", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(),
+    packages=["pvaw"],
     python_requires=">=3.6",
 )
